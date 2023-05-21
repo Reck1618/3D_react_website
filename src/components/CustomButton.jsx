81b83fc5d6d255b1194995fda5af5a476eb2af42
Reck1618 <ritiktyagi1.618@gmail.com>
2023-05-21 22:19:28 +0530

    added button highlight, filter and file picker.

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import state from '../store'
 import { useSnapshot } from "valtio";
 
 const CustomButton = ( { type, title, customStyles, handleClick}) => {
     const snap = useSnapshot(state);
+    const [isHovered, setIsHovered] = useState(false);
     const generateStyle = (type) => {
         if(type === 'filled') {
             return {
                 background: snap.color,
-                color: '#fff'
+                color:'#fff',
+                // on hover
+                transition: 'background-color 0.3s ease-in-out',
+                backgroundColor: isHovered ? '#db6b16' : snap.color,
             }
         }
     }
+
     return (
         <button
             className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
             style={generateStyle(type)}
             onClick={handleClick}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
         >
             {title}
         </button>
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -11,6 +11,27 @@ import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from "../compone
 
 const Customizer = () => {
     const snap = useSnapshot(state);
+    const [file, setFile] = useState('');
+    const [prompt, setPrompt] = useState('');
+    const [generateImg, setGenerateImg] = useState(false);
+    const [activeEditorTab, setActiveEditorTab] = useState('');
+    const [activeFilterTab, setActiveFilterTab] = useState({
+        logoShirt: true,
+        stylishShirt: false,
+    });
+    // show tab content depending on the activeTab
+    const generateTabContent = () => {
+        switch (activeEditorTab) {
+            case 'colorpicker':
+                return <ColorPicker />;
+            case 'filepicker':
+                return <FilePicker />;
+            case 'aipicker':
+                return <AIPicker />;
+            default:
+                return null;
+        }
+    }
     return (
         <AnimatePresence>
             {!snap.intro && (
@@ -26,9 +47,10 @@ const Customizer = () => {
                                     <Tab
                                         key={tab.name}
                                         tab={tab}
-                                        handleClick={() => {}}
+                                        handleClick={() => setActiveEditorTab(tab.name)}
                                     />
                                 ))}
+                                {generateTabContent()}
                             </div>
                         </div>
                     </motion.div>
