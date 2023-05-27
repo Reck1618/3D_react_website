1aa330ea759915b90fe5f04f326be6324d2eebc6
Reck1618 <ritiktyagi1.618@gmail.com>
2023-05-28 02:11:24 +0530

    added backend api

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -48,7 +48,7 @@ const Customizer = () => {
         try {
 
             setGenerateImg(true);
-            const response = await fetch('http://localhost:8080/api/v1/dalle',
+            const response = await fetch('https://project-3js-ai.onrender.com/api/v1/dalle',
                 {
                     method: 'POST',
                     headers: {'Content-Type': 'application/json'},
